e837ed4f689b1c6fcac919cea85c99c7c3c99443
Larry Price <larry.price.dev@gmail.com>
2015-10-15 23:48:00 -0400

    section 1.4

--- main.go
+++ main.go
@@ -4,24 +4,33 @@ import (
   "fmt"
   "net/http"
   "html/template"
+  _ "github.com/mattn/go-sqlite3"
+  "database/sql"
 )
 
 type Page struct {
   Name string
+  DBStatus bool
 }
 
 func main() {
   templates := template.Must(template.ParseFiles("templates/index.html"))
 
+  db, _ := sql.Open("sqlite3", "dev.db")
+
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    p := Page{Name: "Gopher"}
+    p := Page{Name: "Gopher", DBStatus: false}
     if name := r.FormValue("name"); name != "" {
       p.Name = name
     }
 
+    p.DBStatus = db.Ping() == nil
+
     if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
       http.Error(w, err.Error(), http.StatusInternalServerError)
     }
+
+    db.Close()
   })
 
   fmt.Println(http.ListenAndServe(":8080", nil))
