2f4d40cafa760fd59848afe8139665073e9a928f
Larry Price <larry.price.dev@gmail.com>
2015-10-15 21:16:32 -0400

    wrapping up 1.3

--- main.go
+++ main.go
@@ -3,11 +3,25 @@ package main
 import (
   "fmt"
   "net/http"
+  "html/template"
 )
 
+type Page struct {
+  Name string
+}
+
 func main() {
+  templates := template.Must(template.ParseFiles("templates/index.html"))
+
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-  	fmt.Fprintf(w, "Hello, Go Web Development\n")
+    p := Page{Name: "Gopher"}
+    if name := r.FormValue("name"); name != "" {
+      p.Name = name
+    }
+
+    if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
   })
 
   fmt.Println(http.ListenAndServe(":8080", nil))
