c58ed4e585333fc30912eb00e9a949fed74d642d
Larry Price <lrprice@sep.com>
2016-02-01 10:21:32 -0500

    section 3.2

--- main.go
+++ main.go
@@ -15,9 +15,15 @@ import (
   "github.com/yosssi/ace"
 )
 
+type Book struct {
+  PK int
+  Title string
+  Author string
+  Classification string
+}
+
 type Page struct {
-  Name string
-  DBStatus bool
+  Books []Book
 }
 
 type SearchResult struct {
@@ -48,11 +54,13 @@ func main() {
       http.Error(w, err.Error(), http.StatusInternalServerError)
     }
 
-    p := Page{Name: "Gopher"}
-    if name := r.FormValue("name"); name != "" {
-      p.Name = name
+    p := Page{Books: []Book{}}
+    rows, _ := db.Query("select pk,title,author,classification from books")
+    for rows.Next() {
+      var b Book
+      rows.Scan(&b.PK, &b.Title, &b.Author, &b.Classification)
+      p.Books = append(p.Books, b)
     }
-    p.DBStatus = db.Ping() == nil
 
     if err = template.Execute(w, p); err != nil {
       http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -81,10 +89,21 @@ func main() {
       http.Error(w, err.Error(), http.StatusInternalServerError)
     }
 
-    _, err = db.Exec("insert into books (pk, title, author, id, classification) values (?, ?, ?, ?, ?)",
+    result, err := db.Exec("insert into books (pk, title, author, id, classification) values (?, ?, ?, ?, ?)",
                       nil, book.BookData.Title, book.BookData.Author, book.BookData.ID, book.Classification.MostPopular)
 
     if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+
+    pk, _ := result.LastInsertId()
+    b := Book{
+      PK: int(pk),
+      Title: book.BookData.Title,
+      Author: book.BookData.Author,
+      Classification: book.Classification.MostPopular,
+    }
+    if err := json.NewEncoder(w).Encode(b); err != nil {
       http.Error(w, err.Error(), http.StatusInternalServerError)
     }
   })
