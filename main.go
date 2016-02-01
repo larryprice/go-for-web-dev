6e5738954883df8e15be2388dd3de5a19b4037d4
Larry Price <lrprice@sep.com>
2016-02-01 14:10:48 -0500

    section 5.2

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
   "io/ioutil"
   "encoding/xml"
   "strconv"
+  "golang.org/x/crypto/bcrypt"
 
   "github.com/codegangsta/negroni"
   "github.com/goincremental/negroni-sessions"
@@ -26,6 +27,11 @@ type Book struct {
   Author string `db:"author"`
   Classification string `db:"classification"`
   ID string `db:"id"`
+}
+
+type User struct {
+  Username string `db:"username"`
+  Secret []byte `db:"secret"`
 }
 
 type Page struct {
@@ -49,6 +55,7 @@ func initDb() {
   dbmap = &gorp.DbMap{Db: db, Dialect: gorp.SqliteDialect{}}
 
   dbmap.AddTableWithName(Book{}, "books").SetKeys(true, "pk")
+  dbmap.AddTableWithName(User{}, "users").SetKeys(false, "username")
   dbmap.CreateTablesIfNotExists()
 }
 
@@ -85,15 +92,41 @@ func getStringFromSession(r *http.Request, key string) string {
   return strVal
 }
 
+type LoginPage struct {
+  Error string
+}
+
 func main() {
   initDb()
 
   mux := gmux.NewRouter()
 
   mux.HandleFunc("/login", func (w http.ResponseWriter, r *http.Request) {
-    if r.FormValue("register") != "" || r.FormValue("login") != "" {
-      http.Redirect(w, r, "/", http.StatusFound)
-      return
+    var p LoginPage
+    if r.FormValue("register") != "" {
+      secret, _ := bcrypt.GenerateFromPassword([]byte(r.FormValue("password")), bcrypt.DefaultCost)
+      user := User{r.FormValue("username"), secret}
+      if err := dbmap.Insert(&user); err != nil {
+        p.Error = err.Error()
+      } else {
+        http.Redirect(w, r, "/", http.StatusFound)
+        return
+      }
+    } else if r.FormValue("login") != "" {
+      user, err := dbmap.Get(User{}, r.FormValue("username"))
+      if err != nil {
+        p.Error = err.Error()
+      } else if user == nil {
+        p.Error = "No such user found with Username: " + r.FormValue("username")
+      } else {
+        u := user.(*User)
+        if err = bcrypt.CompareHashAndPassword(u.Secret, []byte(r.FormValue("password"))); err != nil {
+          p.Error = err.Error()
+        } else {
+          http.Redirect(w, r, "/", http.StatusFound)
+          return
+        }
+      }
     }
 
     template, err := ace.Load("templates/login", "", nil)
@@ -102,7 +135,7 @@ func main() {
       return
     }
 
-    if err = template.Execute(w, nil); err != nil {
+    if err = template.Execute(w, p); err != nil {
       http.Error(w, err.Error(), http.StatusInternalServerError)
       return
     }
