18e28b836b89124983ed7a612518dbe01891bc60
Larry Price <lrprice@sep.com>
2016-02-01 13:39:56 -0500

    section 5.1

--- main.go
+++ main.go
@@ -90,6 +90,24 @@ func main() {
 
   mux := gmux.NewRouter()
 
+  mux.HandleFunc("/login", func (w http.ResponseWriter, r *http.Request) {
+    if r.FormValue("register") != "" || r.FormValue("login") != "" {
+      http.Redirect(w, r, "/", http.StatusFound)
+      return
+    }
+
+    template, err := ace.Load("templates/login", "", nil)
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+
+    if err = template.Execute(w, nil); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+  })
+
   mux.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
     var b []Book
     if !getBookCollection(&b, getStringFromSession(r, "SortBy"), r.FormValue("filter"), w) {
